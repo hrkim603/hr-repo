8ff43f72c01bb585e1ac4922c7f915c51353b5c4
hrkim603 <101381835+hrkim603@users.noreply.github.com>
2022-03-22 13:18:29 +0900

    Create gcd_lcm.py

---
+++ gcd_lcm.py
@@ -0,0 +1,18 @@
+def gcd(a, b):
+    while(a % b != 0):
+        temp = a % b
+        a = b
+        b = temp
+    return int(b)
+
+def lcm(a, b):
+    return int(a * b // gcd(a, b))
+
+a, b, c = input().split()
+a, b, c = int(a), int(b), int(c)
+
+GCD = gcd(gcd(a,b), c)
+
+LCM = lcm(lcm(a, b), c)
+
+print(GCD, LCM)
